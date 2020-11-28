9e366d2b42eb677360ccf9707ca6e618d118d76d
mahnoorasiff <imahnoor17@gmail.com>
2020-11-28 18:51:53 +0500

    new commit

--- App.js
+++ App.js
@@ -1,203 +1,224 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button}  from 'react-native';
-//import ButtonPanel from "./ButtonPanel";
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.textstyling}  >My name is Moiz Nasir</Text>
-//       <Text style={styles.subtextstyle}> SP18-BCS-089</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-
-class ButtonPanel extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <View>
-        <Button title={this.props.number} onPress={this.props.handler}/>
-        </View>
-    );
-  }
-}
-
+import * as React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Button, FlatList, TextInput,Modal,Alert } from 'react-native';
+import Constants from 'expo-constants';
 
 
 
 export default class App extends React.Component {
-  constructor(props) {
+   constructor(props) {
     super(props);
     this.state = {
-      input: "",
-      gstate: "",
-      output: "",
-      switch:""
+      input1: "",
+      input2: "",
+      amount_saved:"",
+      disc_amount:"",
+      history:"",
+      modalview:false
     };
-    this.handleOperand = this.handleOperand.bind(this);
-
-
-
-  }
-
-
-  
-handleOperand(op) {
-  this.setState({ input: op });
-}
-
-Guess() {
-  const r = Math.floor(Math.random() * 10);
-//   //console.log(r);
-   const v = parseInt(this.state.input, 10);
-   if (r === v) {
-     this.setState({ input: v, setg: r, output: "Congrats, number matched!" });
-   } else {
-     this.setState({ output:"Sorry, wrong guess!"});
-   }
    }
 
-  StartScreen= ()=> {
-  
-    return (
-      <View>
-      <Text style={{ textAlign: 'center', fontSize: 20 }}>
-        Welcome to Guess a Number Game
-      </Text>
-      <Button title="Start Game" onPress={this.setState({switch:"1"})} />
-    </View>
-      );  
-}
-
-
-gameView(){
-<View> 
-      
-
-      <View style={{fontSize:20}}>
-      <Text>{this.state.input}</Text>
-      <Text>{this.state.output}</Text>
-      </View>
-  
-  
-          <View style={styles.ButtonPanel}>
-          <ButtonPanel handler={() => this.handleOperand("AC")} number="AC"/>
-          <ButtonPanel handler={() => this.handleOperand("(x)")} number="(x)"/>
-          <ButtonPanel handler={() => this.handleOperand("/")} number="/"/>
-          <ButtonPanel handler={() => this.handleOperand("%")} number="%"/>
-          </View>
-  
-          <View style={styles.ButtonPanel}>
-          <ButtonPanel handler={() => this.handleOperand("7")} number="7"/>
-          <ButtonPanel handler={() => this.handleOperand("8")} number="8"/>
-          <ButtonPanel handler={() => this.handleOperand("9")} number="9"/>
-          <ButtonPanel handler={() => this.handleOperand("*")} number="*"/>
-          </View>
-  
-        
-          <View style={styles.ButtonPanel}>
-          <ButtonPanel handler={() => this.handleOperand("4")} number="4"/>
-          <ButtonPanel handler={() => this.handleOperand("5")} number="5"/>
-          <ButtonPanel handler={() => this.handleOperand("6")} number="6"/>
-          <ButtonPanel handler={() => this.handleOperand("-")} number="-"/>
-          </View>
-  
-          <View style={styles.ButtonPanel}>
-          <ButtonPanel handler={() => this.handleOperand("1")} number="1" />
-          <ButtonPanel handler={() => this.handleOperand("2")} number="2" />
-          <ButtonPanel handler={() => this.handleOperand("3")} number="3" />
-          <ButtonPanel handler={() => this.handleOperator("+")} number="+" />
-        </View>
-  
-        <View style={styles.ButtonPanel}>
-          <ButtonPanel handler={() => this.handleOperand("0")} number="0"/>
-          <ButtonPanel handler={() => this.handleOperand(".")} number="."/>
-          <ButtonPanel handler={() => this.handleOperator("=")} number="="/>
-          <ButtonPanel handler={() => this.Guess()} number="Guess"/>
-          </View>
-        <Button title="SUBMIT" onPress={()=>this.EndScreen}/>
-  
-        </View>
-  
-}
-
-
-    EndScreen(){
-      
-<Text>Finish</Text>
-        
-
+ input1handle = (value) => {
+    if (parseFloat(value) >= 0 || parseFloat(value) <= 9) {
+      this.setState({input1:value })
     }
+  };
 
-  render() {
-    return (
-      Switching=()=>{
-      <View style={styles.container}>
- <View>{this.StartScreen()}</View> 
-{/* 
-if ({this.state.switch=="1"})
-  return
-  {this.gameView}
-  
- else
- return
- <View >{this.StartScreen()}</View>  */}
-  
-    
-</View>
-      }
-    )
-    ;
+ input2handle = (value) => {
+    if (parseFloat(value) > 0 || parseFloat(value) <= 9) {
+        this.setState({input2:value })
+    }
+  };
+
+
+  calculation = () => {
+
+    const ogprice=this.state.input1
+    const discprice=this.state.input2
+    if (discprice <= 100 && ogprice >= 0 ) {
+      this.setState({amount_saved: ((ogprice * discprice) / 100)}) 
+      this.setState({disc_amount:(ogprice)-((ogprice * discprice) / 100)})  
   }
-return
+  }
+
+
+  saving =()=>{
+    var dash = " | ";
+    var result = "Rs: " + this.state.input1 + dash + this.state.input2 + "% " + dash + "Rs: " +     this.state.disc_amount;
+
+  this.setState({history: [result]});
+
+    this.setState({input1:""});
+    this.setState({input2:""});
+
+    
+  }
+
+  viewHistory = () => {
+    this.setState({modalview:true});
+  }
+
+
+
+
+
+
+
+
+  render(){
+  return (
+    <View style={styles.container}>
+      <View style={styles.input}>
+    <TextInput keyboardType={"number-pad"} placeholder={"Original Price"} value={this.state.input1} onChangeText={this.input1handle} />
+    </View>
+  <View style={styles.input}>
+    <TextInput keyboardType={"number-pad"} placeholder={"Discount"} value={this.state.input2} onChangeText={this.input2handle} />
+  </View>
+  <View style={{flexDirection:"row", marginLeft:-3}}>
+      <TouchableOpacity onPress={() => this.calculation()} style={styles.calculate}>
+          <Text style={{fontSize: 18, color: 'white'}}>COMPUTE</Text>
+        </TouchableOpacity> 
+
+        <TouchableOpacity onPress={() => this.saving()} style={styles.save}>
+          <Text style={{fontSize: 18, color: 'white'}}>SAVE</Text>
+        </TouchableOpacity> 
+  </View>
+  <View style={styles.results}>
+      <Text style={{padding:5, color:"grey", fontSize:15}}>FINAL AMOUNT: {this.state.disc_amount}</Text>
+      <Text style={{padding:5, color:"grey", fontSize:15}}>AMOUNT SAVED: {this.state.amount_saved}</Text>
+    </View>
+
+  <View style={{padding:10}}>
+        <TouchableOpacity onPress={() => this.viewHistory()} style={styles.history}>
+          <Text style={{fontSize: 18, color: 'white'}}>HISTORY</Text>
+        </TouchableOpacity> 
+</View>
+
+<Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalview}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <View>
+            <View style={styles.mdisplay}>
+              <Text style={styles.mhead}>Discount History</Text>
+              <Text style={{fontSize:18}}>Original Price | Discount% | Final Price</Text>
+              <FlatList data={this.state.history}
+                renderItem={({ item }) => { return <Text>{item}</Text> }}
+                keyExtractor={(index) => { return index }} />
+
+              <TouchableOpacity
+                style={{...styles.closebtn,  backgroundColor: "#ff83ff" }}
+                onPress={() => {
+                  this.setState({modalview:false});
+                }}
+              >
+                <Text style={{fontSize:18, color:"white", textAlign:"center"}}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+ </View>
+  
+  );
+  }
 }
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  
+
+  input: {
+    height: 45,
+    width: 270,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 5, 
+    marginBottom:8,
+    fontSize: 18,
+    borderRadius: 10,
+  },
+
+ calculate: {
+    height: 50,
+    width: 120,
+    backgroundColor: '#FA8072',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 10,
+    elevation: 2,
+    marginLeft:10
   },
-
-  textstyling: {
-    color: 'black',
-    fontSize: 32,
-    textAlign:'center',
-    padding:10
-    
-  },
-
-  // subtextstyle:{
-  //   color:'#8ab92d',
-  //   fontSize: 25,
-  //   padding:12,
-  //   textAlign:'center'
-  // }
-
-  // buttonpanel: {
-  //   display: "flex",
-  //   backgroundColor:"#8ab92d",
-  //   flexDirection:"row",
-  // },
-
   
-ButtonPanel:{
-width:"45%",
-// height: "12%",
-padding:"5%",
-display: "flex",
-flexDirection:"row"
+save:{
+    height: 50,
+    width: 120,
+    backgroundColor: '#00FA9A',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginLeft:10
+},
 
-}
+history:{
+    height: 50,
+    width: 130,
+    backgroundColor: '#708090',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginLeft:50
+
+},
+    results: {
+    padding:"3%",  
+    backgroundColor: "#AFEEEE",
+    display:"flex",
+    width:"80%",
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    marginTop:"5%",
+  },
 
 
-});
+  mdisplay: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    height:"80%",
+    paddingTop: 35,
+    paddingBottom: 35,
+    paddingLeft: 10,
+    paddingRight: 10,
+    alignItems: "center",
+  
+  },
+
+ mhead: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingBottom: 10
+  },
 
 
+  closebtn: {
+    backgroundColor: "#F194FF",
+    borderRadius: 5,
+    width: 70,
+    height: 30,
+    marginTop:20,
+    textAlign:"center",
+    justifyContent: 'center'
+  },
+
+  });
